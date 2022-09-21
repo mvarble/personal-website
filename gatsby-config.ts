26ab917d0a895e73fa4ac84a53fe982848de895d
Matthew Varble <matthew@rodent.club>
2022-09-20 17:30:05 -0700

    setup everything to be ready for gh-pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,6 +3,7 @@ import { extendOptions } from '@mvarble/gatsby-plugin-mdx-config';
 import remarkToDeckSchema from '@mvarble/gatsby-theme-presentations/dist/remark-to-deck-schema';
 
 const config: GatsbyConfig = {
+  pathPrefix: '/personal-website',
   plugins: [
     'gatsby-plugin-postcss',
     '@mvarble/gatsby-plugin-mdx-reference',
